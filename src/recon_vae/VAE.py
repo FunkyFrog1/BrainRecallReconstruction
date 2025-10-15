fix: optimize VAE gradient computation and add subp method could be selected in .src/retrieval

src/recon_vae/VAE.py
@@ -1,65 +1,48 @@
 import PIL.Image
-from diffusers.models.autoencoders import AutoencoderKL
-from diffusers.image_processor import VaeImageProcessor
-import torch.nn.functional as F
 import torch
+import torch.nn.functional as F
+from diffusers.image_processor import VaeImageProcessor
+from diffusers.models.autoencoders import AutoencoderKL
 
 
 class VAEProcessor:
     def __init__(self, vae_path='../../vision_backbone/vae', device='cuda', dtype=torch.bfloat16):
-        """
-        初始化VAE处理器
-
-        参数:
-            vae_path: VAE模型路径
-            device: 运行设备
-            dtype: 数据类型
-        """
         self.device = device
         self.dtype = dtype
 
-        # 加载VAE模型
         self.vae = AutoencoderKL.from_pretrained(
             vae_path,
             torch_dtype=self.dtype
         ).to(device).eval()
 
-        # 设置图像处理器
         vae_scale_factor = 2 ** (len(self.vae.config.block_out_channels) - 1)
         self.image_processor = VaeImageProcessor(
             vae_scale_factor=vae_scale_factor,
             vae_latent_channels=self.vae.config.latent_channels
         )
 
-        # 禁用梯度计算
         for param in self.vae.parameters():
             param.requires_grad = False
 
     def encode_to_latent_dist(self, x):
-        # 预处理图像
         x = self.image_processor.preprocess(x)
 
         with torch.no_grad():
-            # 确保输入数据类型正确
             if x.dtype != self.dtype:
                 x = x.to(self.dtype)
 
-            # 使用VAE编码器
             posterior = self.vae.encode(x).latent_dist
             latent_sample = posterior.sample()
 
             return posterior, latent_sample
 
     def decode_from_latent(self, latent, post_process=True):
-        # 确保潜变量数据类型正确
         if latent.dtype != self.dtype:
             latent = latent.to(self.dtype)
 
         decoded = self.vae.decode(latent).sample
 
-        # 后处理
         if post_process:
-            # 返回张量
             decoded = self.image_processor.postprocess(
                 decoded,
                 output_type="pt",
@@ -102,9 +85,11 @@ def test_vae():
         # 1. 编码为潜在分布
         posterior, latent_sample = vae_processor.encode_to_latent_dist(x)
         print(f"潜在变量形状: {latent_sample.shape}")
+        print(latent_sample)
 
         # 2. 从潜变量解码回图像
-        reconstructed_image = vae_processor.decode_from_latent(latent_sample)
+        reconstructed_image = vae_processor.decode_from_latent(latent_sample, post_process=False)
+        print(reconstructed_image)
         print(f"重建图像形状: {reconstructed_image.shape}")
 
         # 3. 计算重建质量指标
@@ -119,6 +104,7 @@ def test_vae():
 
     except FileNotFoundError:
         print("测试图像未找到，使用随机张量进行测试")
+
 
 def visualize_comparison(original, reconstructed, mse_loss, psnr):
     """

src/retrieval/data_eeg.py
@@ -174,7 +174,10 @@ class EEGDataset(Dataset):
         ]
         self.process_transform = transforms.Compose(process_term)
 
-        self.match_label = np.ones((self.trial_all_subjects, self.per_trials), dtype=int)
+        if self.config['data']['single_uncertainty_aware']:
+            self.match_label = np.ones((self.trial_all_subjects, self.per_trials), dtype=int)
+        elif self.config['data']['uncertainty_aware']:
+            self.match_label = np.ones(self.trial_all_subjects, dtype=int)
 
         if os.path.exists(features_filename):
             saved_features = torch.load(features_filename, weights_only=False)
@@ -328,7 +331,7 @@ class EEGDataset(Dataset):
 
         match_label = self.match_label[index]
 
-        if self.config['data']['uncertainty_aware']:
+        if self.config['data']['single_uncertainty_aware']:
             img_features = []
             for trial_n in range(self.per_trials):
                 if self.mode == 'train':
@@ -343,6 +346,19 @@ class EEGDataset(Dataset):
 
                 img_features.append(self.img_features[tag][img_path])
             img_features = torch.stack(img_features)
+
+        elif self.config['data']['uncertainty_aware']:
+            if self.mode == 'train':
+                if match_label == 0:
+                    tag = 'low'
+                elif match_label == 2:
+                    tag = 'high'
+                else:
+                    tag = 'medium'
+            else:
+                tag = 'medium'
+            img_features = self.img_features[tag][img_path]
+
         else:
             img_features = self.img_features[img_path]
 

src/retrieval/eeg_backbone.py
@@ -72,6 +72,35 @@ class Ours(nn.Module):
         x = x.view(x.shape[0], n, self.input_dim)
         x = self.model(x)
         return x
+class Ours_bn(nn.Module):
+    def __init__(self, z_dim, c_num, timesteps, drop_proj=0.2):
+        super().__init__()
+        self.z_dim = z_dim
+        self.c_num = c_num
+        self.timesteps = timesteps
+
+        self.input_dim = self.c_num * (self.timesteps[1] - self.timesteps[0])
+        proj_dim = z_dim
+
+        self.model = nn.Sequential(nn.Linear(self.input_dim, proj_dim),
+                                   ResidualAdd(nn.Sequential(
+                                       nn.GELU(),
+                                       nn.Linear(proj_dim, proj_dim),
+                                       nn.Dropout(drop_proj),
+                                   )),)
+        self.bn = nn.BatchNorm1d(proj_dim)
+
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        self.softplus = nn.Softplus()
+
+    def forward(self, x):
+        B, n, c, d = x.shape
+        x = x.view(x.shape[0], n, self.input_dim)
+        x = self.model(x)
+        x = x.view(B * n, x.shape[-1])
+        x = self.bn(x)
+        x = x.view(B, n, x.shape[-1])
+        return x
 
 
 from models.MoE import MoELayer
@@ -85,19 +114,30 @@ class Ours3(nn.Module):
         self.input_dim = self.c_num * (self.timesteps[1] - self.timesteps[0])
         proj_dim = z_dim
 
-        self.model = nn.Sequential(nn.Linear(self.input_dim, proj_dim),
+        # self.model = nn.Sequential(nn.Linear(self.input_dim, proj_dim),
+        #                            ResidualAdd(nn.Sequential(
+        #                                nn.GELU(),
+        #                                MoELayer(proj_dim, proj_dim),
+        #                                nn.GELU(),
+        #                                nn.Dropout(drop_proj),
+        #                            )),
+        #                            nn.LayerNorm(proj_dim))
+        self.model = nn.Sequential(nn.LayerNorm(self.input_dim),
+                                   nn.Linear(self.input_dim, proj_dim),
+                                   nn.Dropout(drop_proj/2),
                                    ResidualAdd(nn.Sequential(
+                                       nn.LayerNorm(proj_dim),
                                        nn.GELU(),
-                                       MoELayer(proj_dim, proj_dim),
-                                       nn.GELU(),
+                                       nn.Linear(proj_dim, proj_dim),
                                        nn.Dropout(drop_proj),
                                    )),
-                                   nn.LayerNorm(proj_dim))
+                                   nn.LayerNorm(proj_dim),)
         self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
         self.softplus = nn.Softplus()
 
     def forward(self, x):
-        x = x.view(x.shape[0], self.input_dim)
+        B, n, c, d = x.shape
+        x = x.view(x.shape[0], n, self.input_dim)
         x = self.model(x)
         return x
 

src/retrieval/train.py
@@ -17,7 +17,7 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import torch.nn.functional as F
 ##import user lib
 from data_eeg import load_eeg_data
-from utils import update_config, instantiate_from_config, get_device, ClipLoss, mixco_data
+from utils import update_config, instantiate_from_config, get_device, ClipLoss, mixco_data, marginal_cosine_similarity_loss
 import time
 device = get_device('auto')
 
@@ -47,8 +47,13 @@ class PLModel(pl.LightningModule):
 
         self.z_dim = self.config['models']['brain']['params']['z_dim']
 
-        self.sim = np.ones((len(train_loader.dataset), 80))
-        self.match_label = np.ones((len(train_loader.dataset), 80), dtype=int)
+        if self.config['data']['single_uncertainty_aware']:
+            self.sim = np.ones((len(train_loader.dataset), 80))
+            self.match_label = np.ones((len(train_loader.dataset), 80), dtype=int)
+        else:
+            self.sim = np.ones(len(train_loader.dataset))
+            self.match_label = np.ones(len(train_loader.dataset), dtype=int)
+
         self.alpha = config['alpha']
         self.gamma = 0.3
 
@@ -63,19 +68,26 @@ class PLModel(pl.LightningModule):
         # img = batch['img']
 
         # img_z = batch['img_features']
-        img_z = batch['img_features'][:, :eeg.shape[1], :]
-        print(img_z.shape)
+        if self.config['data']['single_uncertainty_aware']:
+            img_z = batch['img_features'][:, :eeg.shape[1], :]
+        else:
+            img_z = batch['img_features']
+
 
         eeg_z = self.brain(eeg)
-
-        img_z = img_z / img_z.norm(dim=-1, keepdim=True)
 
         logit_scale = self.brain.logit_scale
         logit_scale = F.softplus(logit_scale)
 
-        loss, _ = self.criterion(eeg_z.mean(dim=1), img_z.mean(dim=1), logit_scale)
+        img_z = img_z / img_z.norm(dim=-1, keepdim=True)
+        if self.config['data']['single_uncertainty_aware']:
+            loss, logits_per_image = self.criterion(eeg_z.mean(dim=1), img_z.mean(dim=1), logit_scale)
+        else:
+            loss, logits_per_image = self.criterion(eeg_z.mean(dim=1), img_z, logit_scale)
 
-        if self.config['data']['uncertainty_aware']:
+        loss = loss
+
+        if self.config['data']['single_uncertainty_aware']:
             for trial_n in range(eeg_z.shape[1]):
                 _, logits_per_image = self.criterion(eeg_z[:, trial_n, :], img_z[:, trial_n, :], logit_scale)
                 diagonal_elements = torch.diagonal(logits_per_image).cpu().detach().numpy()
@@ -97,7 +109,29 @@ class PLModel(pl.LightningModule):
                 self.sim[idx, trial_n] = batch_sim
                 self.match_label[idx, trial_n] = match_label
 
-        return eeg_z.mean(dim=1), img_z.mean(dim=1), loss
+            return eeg_z.mean(dim=1), img_z.mean(dim=1), loss
+
+        if self.config['data']['uncertainty_aware']:
+            diagonal_elements = torch.diagonal(logits_per_image).cpu().detach().numpy()
+            gamma = self.gamma
+
+            batch_sim = gamma * diagonal_elements + (1 - gamma) * self.sim[idx]
+
+            mean_sim = np.mean(batch_sim)
+            std_sim = np.std(batch_sim, ddof=1)
+            match_label = np.ones_like(batch_sim)
+            z_alpha_2 = norm.ppf(1 - self.alpha / 2)
+
+            lower_bound = mean_sim - z_alpha_2 * std_sim
+            upper_bound = mean_sim + z_alpha_2 * std_sim
+
+            match_label[diagonal_elements > upper_bound] = 0
+            match_label[diagonal_elements < lower_bound] = 2
+
+            self.sim[idx] = batch_sim
+            self.match_label[idx] = match_label
+
+            return eeg_z.mean(dim=1), img_z, loss
 
     def training_step(self, batch, batch_idx):
         batch_size = batch['idx'].shape[0]
@@ -132,13 +166,11 @@ class PLModel(pl.LightningModule):
             self.all_true_labels = []
 
             # counter = Counter(self.match_label)
-            print(self.match_label.flatten())
             counter = Counter(self.match_label.flatten().tolist())
 
             count_dict = dict(counter)
             key_mapping = {0: 'low', 1: 'medium', 2: 'high'}
             count_dict_mapped = {key_mapping[k]: v for k, v in count_dict.items()}
-            print(count_dict_mapped)
             self.log_dict(count_dict_mapped, on_step=False, on_epoch=True, logger=True, sync_dist=True)
             self.trainer.train_dataloader.dataset.match_label = self.match_label
         return loss
@@ -300,7 +332,7 @@ def run_experiment(args):
         config['seed'] = seed
         config['alpha'] = alpha
         config['timesteps'] = [start_time, end_time]
-        config['info'] = f'-subp{alpha}-[{start_time},{end_time}]-dropout0.2'
+        config['info'] = f'-ubp{alpha}-[{start_time},{end_time}]-dropout0.2'
 
         result = main(config, yaml)
         return (eeg_backbone, vision_backbone, seed, sub, "SUCCESS", result)
@@ -328,7 +360,6 @@ def run_experiment_with_retry(params, max_retries=30):
     # 所有重试都失败
     return ("", "", "", "", "FAILED", "All retries failed")
 
-
 if __name__ == "__main__":
     eeg_backbones = ['Ours']
     vision_backbones = [('ViT-B-32', 512)]
@@ -336,7 +367,7 @@ if __name__ == "__main__":
     subs = [0, 1]
     start_time = [250]
     end_time = [600]
-    alpha = [0.30]
+    alpha = [0.3]
     param_combinations = list(product(eeg_backbones, vision_backbones, seeds, subs, start_time, end_time, alpha))
 
     print(f"总共 {len(param_combinations)} 个实验")